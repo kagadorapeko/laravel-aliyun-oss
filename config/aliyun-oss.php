916d1444d807252e34b8baff4fe1355db2ba8ea6
Kaga Dorapeko <kagadorapeko@gmail.com>
2022-03-25 10:30:22 +0800

    v1.0.0

---
+++ config/aliyun-oss.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'bucket' => env('ALIYUN_OSS_BUCKET'),
+    'region_id' => env('ALIYUN_OSS_REGION_ID'),
+    'access_key_id' => env('ALIYUN_OSS_ACCESS_KEY_ID'),
+    'access_key_secret' => env('ALIYUN_OSS_ACCESS_KEY_SECRET'),
+];
